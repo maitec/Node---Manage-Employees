bc4bf05e83ef0774d96fedcd5af77d21cdb5a443
maitec <maria.maitena@hotmail.com>
2019-07-15 18:26:23 -0300

    funcion editar

--- cliente/index.js
+++ cliente/index.js
@@ -1,30 +1,32 @@
+let tableRows = [];
+
+
 const form = document.querySelector("form");
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const address = document.getElementById('address');
-const phone = document.getElementById('phone');
+const editName = document.getElementById('editName');
+const editEmail = document.getElementById('editEmail');
+const editAddress = document.getElementById('editAddress');
+const editPhone = document.getElementById('editPhone');
 
 fetch('http://localhost:3000/api/users')
-.then(res => res.json())
-.then(data =>{
+    .then(res => res.json())
+    .then(data => {
 
-    const tableRows = data.map(function(u){
-        return `<tr id="${u.id}">
+        tableRows = data.map(function (u) {
+            return `<tr id="${u.id}">
         <td><input type="checkbox"></td>
         <td>${u.name}</td>
         <td>${u.email}</td>
         <td>${u.address}</td>
         <td>${u.phone}</td>
         <td>
-            <i class="material-icons edit" data-toggle="tooltip" title="Edit"></i>
+            <i class="material-icons edit" title="Edit" data-toggle="modal" data-target="#editEmployee" onclick=editar(${u.id})></i>
             <i class="material-icons delete" title="Delete" data-toggle="modal" data-target="#deleteEmployee" onclick=eliminar(${u.id})></i>
         </td>
     </tr>`
-    });
+        });
 
-    document.querySelector('tbody').innerHTML = tableRows.join('');
-
-})
+        document.querySelector('tbody').innerHTML = tableRows.join('');
+    })
 
 
 // form.onsubmit = function(event){
@@ -35,15 +37,53 @@ fetch('http://localhost:3000/api/users')
 //     }
 // }  
 
+function editar(id) {
+    fetch(`http://localhost:3000/api/users`)
+        .then(res => res.json())
+        .then(data => {
+
+            data.forEach(registro => {
+                if (registro.id === id) {
+                    editName.value = registro.name;
+                    editEmail.value = registro.email;
+                    editAddress.value = registro.address;
+                    editPhone.value = registro.phone;
+                }
+                document.getElementById('saveButton').onclick = function editarDefinitivo() {
+                    const usuarioEditado = {
+                        name: editName.value,
+                        email: editEmail.value,
+                        address: editAddress.value,
+                        phone: editPhone.value
+                    }
+                    //console.log("el nombre editado es ", usuarioEditado);
+
+                    fetch(`http://localhost:3000/api/users/${id}`, {
+                        method: 'put',
+                        body: JSON.stringify(usuarioEditado),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data)
+                            const elementoEditado = document.getElementById('id');
+                            console.log("el elemeto editado es ",elementoEditado );
+                            
+                        })
+                }
+            })
+        })
+}
+
 
 function eliminar(id) {
-    
-    document.getElementById('deleteButton').onclick = function eliminarDefinitivo(){
+
+    document.getElementById('deleteButton').onclick = function eliminarDefinitivo() {
         fetch(`http://localhost:3000/api/users/${id}`, { method: 'delete' })
-        .then(res => {
-          document.getElementById(id).remove();
-        });
+            .then(res => {
+                document.getElementById(id).remove();
+            });
     };
-  
-  }
-  
+}
--- servidor/index.js
+++ servidor/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 
 const app = express();
 
@@ -8,24 +8,40 @@ app.use(cors());
 app.use(express.json());
 
 const usuarios = [
-    {id: 1, name: "Bruce Wayne 1", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111},
-    {id: 2, name: "Bruce Wayne 2", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111},
-    {id: 3, name: "Bruce Wayne 3", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111},
-    {id: 4, name: "Bruce Wayne 4", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111},
-    {id: 5, name: "Bruce Wayne 5", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111},
-    {id: 6, name: "Bruce Wayne 6", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111},
-    {id: 7, name: "Bruce Wayne 7", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111},
-    {id: 8, name: "Bruce Wayne 8", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111}
+    { id: 1, name: "Bruce Wayne 1", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111 },
+    { id: 2, name: "Bruce Wayne 2", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111 },
+    { id: 3, name: "Bruce Wayne 3", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111 },
+    { id: 4, name: "Bruce Wayne 4", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111 },
+    { id: 5, name: "Bruce Wayne 5", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111 },
+    { id: 6, name: "Bruce Wayne 6", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111 },
+    { id: 7, name: "Bruce Wayne 7", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111 },
+    { id: 8, name: "Bruce Wayne 8", email: "bruce@wayneenterpreises.com", address: "Gotham City", phone: 08009991111 }
 ];
 
-app.get('/api/users', function(req,res){
+app.get('/api/users', function (req, res) {
     res.json(usuarios);
 })
 
-app.delete('/api/users/:id', function(req,res){
-    console.log(req.params.id);
-    for(let i=0; i<usuarios.length; i++){
-        if(usuarios[i].id == req.params.id){
+app.put('api/users/:id', function (req, res) {
+    const id = req.params.id;
+    console.log("el id es ", id);
+    const usuarioEditado = req.body;
+    console.log(req.body)
+    usuarios.forEach(usuario => {
+        if (usuario.id === id) {
+            usuario.name = usuarioEditado.name;
+            usuario.email = usuarioEditado.email;
+            usuario.address = usuarioEditado.address;
+            usuario.phone = usuarioEditado.phone;
+            return res.json(usuario)
+        }
+    })
+})
+
+app.delete('/api/users/:id', function (req, res) {
+    //console.log(req.params.id);
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].id == req.params.id) {
             usuarios.splice(i, 1);
             i--;
         }
